055b49d03f75bc23530d59f0d2f43dd258345569
lucas <lucas@git.com>
2025-12-31 08:17:18 -0300

    Correção build Vercel: removendo swcMinify

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const withPWA = withPWAInit({
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  swcMinify: true,
+
   disable: process.env.NODE_ENV === "development",
   workboxOptions: {
     disableDevLogs: true,
